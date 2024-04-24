3d1276cabeca5cd64f18a7299c4b1db0f3702323
Liamlunkamba <liamlunkamba12@gmail.com>
2024-04-23 22:57:43 -0500

    myAssignment

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
-const firstNAME = "Liam
-const lastName = "Lunkamba"
-console.log(`Hello World my name is ${firstNAME} ${lastName}`)
+const firstNAME = "Liam";
+const lastName = "Lunkamba";
+const countryName = "from DR Congo";
+console.log(`Hello World my name is ${firstNAME} ${lastName} ${countryName}`);
